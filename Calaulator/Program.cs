9ae77801dea868acdc0c185b44e0fcea5e0a662c
Jorino. <phwn26@gmail.com>
2024-10-18 11:14:55 +0700

    first commit

---
+++ Calaulator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Calaulator;
+using CalculatorLibrary;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = 2;
+        int b = 3;
+        int c = CalculatorLibrary.Add(a,b);
+        Console.WriteLine($"Result from Add({a},{b}) is {c}");
+    }
+}
---
+++ CalculatorLibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace CalculatorLibrary;
+
+public static class CalculatorLibrary
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
